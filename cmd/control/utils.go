148c53ad585b991a2e59ee5c4a8d692414526ae0
0x1eef <0x1eef@protonmail.com>
2025-10-18 02:16:29 +0000

    Organize utils.go

--- cmd/control/utils.go
+++ cmd/control/utils.go
@@ -5,19 +5,20 @@ import (
 	"os"
 )
 
-func println(msg string, args ...any) {
-	printf(msg+"\n", args...)
-}
-
-func fatalln(msg string, args ...any) {
-	fatalf(msg+"\n", args...)
-}
-
 func printf(msg string, args ...any) {
 	fmt.Printf(fmt.Sprintf("control: %s", msg), args...)
+}
+
+func println(msg string, args ...any) {
+	printf(msg+"\n", args...)
 }
 
 func fatalf(msg string, args ...any) {
 	fmt.Fprintf(os.Stderr, fmt.Sprintf("control: %s", msg), args...)
 	os.Exit(1)
 }
+
+func fatalln(msg string, args ...any) {
+	fatalf(msg+"\n", args...)
+}
+
