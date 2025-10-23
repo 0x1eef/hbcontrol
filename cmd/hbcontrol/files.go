cc080b6ab0c8e6ef9f911458118225c700e9bde1
0x1eef <0x1eef@protonmail.com>
2025-10-23 02:52:42 +0000

    Tweaks

--- cmd/hbcontrol/files.go
+++ cmd/hbcontrol/files.go
@@ -8,12 +8,14 @@ func isRegularFile(path string) bool {
 	stat, err := os.Lstat(path)
 	if err != nil {
 		return false
+	} else {
+		mode := stat.Mode()
+		return mode.IsRegular()
 	}
-	mode := stat.Mode()
-	return mode.IsRegular()
 }
 
 func follow(path string) string {
+	const max = 20
 	if !options.followSymlinks {
 		return path
 	} else if isRegularFile(path) {
@@ -21,7 +23,6 @@ func follow(path string) string {
 	} else {
 		p, err := os.Readlink(path)
 		for i := 0; !isRegularFile(p); i++ {
-			const max = 20
 			p, err = os.Readlink(p)
 			if err != nil || i == max {
 				break
