597681dcaf343b459c10fc4811b1f62e7ed28e6c
0x1eef <0x1eef@protonmail.com>
2025-10-23 02:49:31 +0000

    Concise

--- cmd/hbcontrol/files.go
+++ cmd/hbcontrol/files.go
@@ -4,10 +4,6 @@ import (
 	"os"
 )
 
-const (
-	maxLinkDepth = 25
-)
-
 func isRegularFile(path string) bool {
 	stat, err := os.Lstat(path)
 	if err != nil {
@@ -20,22 +16,21 @@ func isRegularFile(path string) bool {
 func follow(path string) string {
 	if !options.followSymlinks {
 		return path
-	}
-	if isRegularFile(path) {
-		return path
-	}
-
-	p, err := os.Readlink(path)
-	for i := 0; !isRegularFile(p); i++ {
-		p, err = os.Readlink(p)
-		if err != nil || i == maxLinkDepth {
-			break
-		}
-	}
-
-	if err != nil {
+	} else if isRegularFile(path) {
 		return path
 	} else {
-		return p
+		p, err := os.Readlink(path)
+		for i := 0; !isRegularFile(p); i++ {
+			const max = 20
+			p, err = os.Readlink(p)
+			if err != nil || i == max {
+				break
+			}
+		}
+		if err != nil {
+			return path
+		} else {
+			return p
+		}
 	}
 }
